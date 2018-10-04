7604b2da0c4528656386c843bbdc319e5b576e21
teresa myers <teresamarie04@gmail.com>
2018-10-04 03:41:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,3 +36,11 @@ def valid_move?(board, index)
     false
   end
 end
+
+   def valid_move?(board, index)
+   if index.between?(0, 8) && position_taken?(board, index) == false
+     true
+   else
+     false
+   end
+ end
