57f2f0ea5c0a3b0a9c653ff0b1183ecc4115cb2a
teresa myers <teresamarie04@gmail.com>
2018-10-04 03:35:34 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,65 @@
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+end
+
+ def input_to_index(input)
+   index = input.to_i - 1
+ end
+
+ def move (array, index, character = "X")
+   array[index] = character
+ end
+
+def valid_move?(board, index)
+   if index.between?(0, 8) && position_taken?(board, index) == false
+     true
+   else
+     false
+   end
+ end
+
+ def position_taken?(board, position)
+    taken = nil
+    if board[position] == " "
+      taken = false
+    elsif board[position] == ""
+      taken = false
+    elsif board[position] == nil
+      taken = false
+    else
+      taken = true
+    end
+    taken
+  end
+
+ def position_taken?(board, position)
+     taken = nil
+     if board[position] == " "
+       taken = false
+     elsif board[position] == ""
+       taken = false
+     elsif board[position] == nil
+       taken = false
+     else
+       taken = true
+     end
+     taken
+   end
+
+   def valid_move?(board, index)
+   if index.between?(0, 8) && position_taken?(board, index) == false
+     true
+   else
+     false
+   end
+ end
